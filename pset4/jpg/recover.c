3fdb5d7b4eedc00134b7122f120504ceae8b9aa2
Kevin Perez <unknotted26@gmail.com>
2016-07-24 10:17:17 +0000

    backtraces removed. Pset complete.

--- pset4/jpg/recover.c
+++ pset4/jpg/recover.c
@@ -59,69 +59,6 @@ int getFileSize(FILE * file)
   return fileSize;
 }
 
-int countZeroBytes(FILE* file)
-{
-  int zeroBytes = 0;
-  while(fgetc(file) == 0)
-    ++zeroBytes;
-  return zeroBytes;
-}
-
-int countNonZeroBytes(FILE* file)
-{
-  int amountOfBytes = 0;
-  while(fgetc(file) != 0)
-    ++amountOfBytes;
-
-  return amountOfBytes;
-}
-
-int findNextJPEG(FILE* file)
-{
-  int c;
-  do
-  {
-    c = fgetc(file);
-  } while(c != 0xff);
-  ungetc(c, file);
-  return 1;
-}
-
-char byteCheck(char byte)
-{
-  int arrSize = sizeof(JPEGBytes);
-  for(int i = 0; i < arrSize; ++i)
-  {
-    if(byte == JPEGBytes[i])
-      return byte;
-  }
-  return 0;
-}
-/*
-void createJPEG(FILE* infile, char* fileName, uint8_t fourthMETAByte)
-{
-  FILE * outfile = fopen(fileName, "w");
-  uint8_t meta[] = { 0xff, 0xd8, 0xff, 0xe0 };
-  fwrite(meta, 1, 4, outfile);
-  printf("Sizeof(fgetc): %d\n", fgetc(infile));
-  uint8_t c = fgetc(infile);
-  uint8_t previous;
-  //JPEGIdentifier id;
-  //fread(&id, sizeof(JPEGIdentifier), 1, outfile);
-  while(c != 0xd9 && previous != 0xff)
-  {
-    //fwrite(&id, sizeof(JPEGIdentifier), 1, outfile);
-    fputc(c, outfile);
-    previous = c;
-    //fwrite(c, 1, 1, outfile);
-    c = fgetc(infile);
-    printf("0x%x\n", c);
-  }
-  fputc(0xd9, outfile);
-  printf("DONE PRINTING!\n");
-  fclose(outfile);
-}
-*/
 int main(void)
 {
     FILE * infile = fopen("card.raw", "r");
@@ -133,41 +70,32 @@ int main(void)
     
     size_t infileSize = getFileSize(infile);
     
-    unsigned int readCounter = 0;
-    unsigned int JPEGFileCounter = 0;
+    unsigned int blockCounter = 0;
+    unsigned int JPEGFileCounter = 0;  // Used for naming 
     FILE * outfile = NULL;
-    char JPEGName[7];
+    char JPEGName[7];        // stores JPG file names
     
-    while(readCounter < infileSize/512)
+    while(blockCounter < infileSize / 512)
     {
-      uint8_t JPEG[512];
+      uint8_t JPEG[512]; // store image bytes
       fread(&JPEG, 1, 512, infile);
-      if(JPEG[0] == 0xff && JPEG[1] == 0xd8 && JPEG[2] == 0xff)
+      
+      if (JPEG[0] == 0xff && JPEG[1] == 0xd8 && JPEG[2] == 0xff) // new image
       {
-        sprintf(JPEGName,"%03d.jpg", JPEGFileCounter);
+        if(outfile != NULL)  // Done here to avoid previous image backtrace error
+          fclose(outfile);
+        
+        sprintf(JPEGName,"%03d.jpg", JPEGFileCounter); // creating file name
         outfile = fopen(JPEGName, "w");
-        fwrite(&JPEG, 1, 512, outfile);
+        fwrite(&JPEG, 1, 512, outfile); 
         ++JPEGFileCounter;
       }
-      else if(outfile != NULL)
+      else if (outfile != NULL)
       {
-        int open = 1;
-        size_t bytesToWrite = 512;
-        for(int i = 1; i < 512; ++i)
-        {
-          if(JPEG[i-1] == 0xff && JPEG[i] == 0xd9)
-          {
-            bytesToWrite = i+1+1; 
-            fwrite(&JPEG, 1, bytesToWrite, outfile);
-            fclose(outfile);
-            open = 0;
-            i = 512;
-          }
-        }
-        if(open)
-          fwrite(&JPEG, 1, bytesToWrite, outfile);
-      } // else-if
-      ++readCounter;
+          fwrite(&JPEG, 1, 512, outfile);
+      } 
+      
+      ++blockCounter;
     }
     
     fclose(infile);
